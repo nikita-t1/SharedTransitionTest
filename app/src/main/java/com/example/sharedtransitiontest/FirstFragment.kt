a83cfb4849d75080a26f5c0b3503a2a53fe34b5e
nikita-t1 <nikita.t2@gmx.de>
2020-05-03 17:30:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sharedtransitiontest/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.example.sharedtransitiontest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.transition.TransitionInflater
+import kotlinx.android.synthetic.main.fragment_first.*
+import java.util.concurrent.TimeUnit
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<CardView>(R.id.card).setOnClickListener {
+            val extras = FragmentNavigatorExtras(mainText to "mainText", startTime to "startTime", endTime to "endTime", card to "card")
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, null,null, extras)
+        }
+    }
+}
